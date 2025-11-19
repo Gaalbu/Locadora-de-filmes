0a5efea232dc9558e496e4aee98a62851802e016
Gaalbu <gabrielalencarem@gmail.com>
2025-11-19 19:25:15 -0300

    feat: adicionando esqueleto base do projeto.

---
+++ locadoraDeFilmes/src/main/java/projetoDTI/Leitor.java
@@ -0,0 +1,22 @@
+package projetoDTI;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
+
+public class Leitor {
+    //Declaração de leitor para reutilização futura no código
+    private BufferedReader leitorIO;
+
+    //Declarando um leitor para evitar problemas de buffer.
+    public Leitor(){
+        this.leitorIO = new BufferedReader(new InputStreamReader(System.in));
+    }
+    
+    //Método de leitura IO.
+    public String IOinput() throws IOException{
+        return leitorIO.readLine();
+    }
+}
--- locadoraDeFilmes/src/main/java/projetoDTI/Menu.java
+++ locadoraDeFilmes/src/main/java/projetoDTI/Menu.java
@@ -1,5 +1,5 @@
 package projetoDTI;
-
+import java.io.IOException;
 
 /*
 Entendendo que é uma aplicação de console, a classe menu
@@ -9,5 +9,79 @@ deixando a classe App mais limpa.
 */
 
 public class Menu {
+    Leitor leitorInputs = new Leitor();
     
+    public void executar(){
+        int opcao = 0;
+        
+        // loop padrão da aplicação de console.
+        do{
+            System.out.println("=====================");
+            System.out.println("1. Listar todos os filmes cadastrados");
+            System.out.println("2. Buscar um filme por ID");
+            System.out.println("3. Cadastrar um novo filme");
+            System.out.println("4. Atualizar informações do filme");
+            System.out.println("5. Deletar filme cadastrado");
+
+            //Verificação se a entrada está correta.
+            try {
+                opcao = Integer.parseInt(leitorInputs.IOinput());
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            
+            //Esqueleto das opções.
+            switch (opcao) {
+                case 1:
+                    //Listagem de todos os filmes cadastrados
+                    try {
+                        //locadora.listarCadastros();
+                    } catch (Exception e) {
+                        
+                    }
+
+                    break;
+                case 2:
+                    //Buscar filme por ID.
+                    try{
+                       //locadora.buscarCadastro();
+                    }catch (Exception e){
+                        
+                    }
+
+                    break;
+                case 3:
+                    //Cadastro de filmes.
+                    try{
+                        //locadora.cadastrarFilme();
+                    }catch (Exception e){
+                        
+                    }
+
+                    break;
+                case 4:
+                    //Alteração em um cadastro.
+                    try {
+                        //locadora.alterarCadastro();
+                    } catch (Exception e) {
+                        System.out.println("Não há pets cadastrados.");
+                    }
+                    break;
+                case 5:
+                    //Deletar um cadastro.
+                    try{
+                        //locadora.excluirCadastro();
+                    }catch(Exception e){
+                        System.out.println("Não há pets cadastrados/Não há pets que atinjam os critérios. Motivo:" + e.getMessage());
+                    }
+
+                    break;
+                case 6:
+                    
+                    
+                    break;
+            }
+
+        }while (opcao != 6);
+    }
 }
