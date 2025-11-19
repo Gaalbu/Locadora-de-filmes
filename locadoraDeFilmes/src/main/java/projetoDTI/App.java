8525b97a1687f4fe07b92c1fb8f868af16fb49ce
Gaalbu <gabrielalencarem@gmail.com>
2025-11-19 19:00:27 -0300

    init: inicializando o projeto e configurando dependências.

---
+++ locadoraDeFilmes/src/main/java/projetoDTI/App.java
@@ -0,0 +1,12 @@
+package projetoDTI;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Menu locadoraMenu = new Menu();
+
+        //locadoraMenu.executar();
+        
+    }
+}
---
+++ locadoraDeFilmes/src/main/java/projetoDTI/Menu.java
@@ -0,0 +1,13 @@
+package projetoDTI;
+
+
+/*
+Entendendo que é uma aplicação de console, a classe menu
+funcionará para organizar as funcionalidades do projeto,
+deixando a classe App mais limpa.
+
+*/
+
+public class Menu {
+    
+}
